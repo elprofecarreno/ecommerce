3fd2abe99693de24ae0a000ea9e77826640953f4
Cristian Carreño <elprofecarreno@gmail.com>
2023-06-03 00:10:55 -0400

    add delete and update region

--- web_ecommerce/app/views/regiones_view.py
+++ web_ecommerce/app/views/regiones_view.py
@@ -10,7 +10,7 @@ def app_regiones_view(app):
     API_REST_HOST = os.getenv('API_REST_HOST')
     API_REST_PORT = os.getenv('API_REST_PORT')
 
-    @app.route('/admin/regiones',  methods=['GET', 'POST'])
+    @app.route('/admin/regiones',  methods=['GET', 'POST', 'DELETE'])
     def regiones_view():
         print('METHOD: ', request.method)
         title = 'E-commerce ADMIN'
@@ -28,8 +28,35 @@ def app_regiones_view(app):
                     {'href': '/admin/index', 'title': 'Provinces', 'icon' : 'mdi mdi-image-filter-hdr'},
                     {'href': '/admin/regiones', 'title': 'Regiones', 'icon' : 'mdi mdi-earth'},
                 ]
+
+        message = {
+            'type': 'warning d-none',
+            'text': ''
+        }
         if request.method == 'POST':
-            regiones_create(request)
+            transaction = request.form.get('transaction')
+            print('transaction: ', transaction)
+            if transaction == 'create':
+                regiones_create(request)
+                message = {
+                    'type': 'success',
+                    'text': 'Created Region'
+                }                
+            elif transaction == 'update':
+                regiones_update(request)
+                message = {
+                    'type': 'success',
+                    'text': 'Updated Region ' +  request.form.get('id')
+                }                  
+            elif transaction == 'delete':
+                id = request.form.get('id')
+                if id is not None:
+                    regiones_delete(id)
+                    message = {
+                        'type': 'success',
+                        'text': 'Deleted Region ' + id
+                    }                      
+               
         regiones = []
         try:
             url = 'http://{API_REST_HOST}:{API_REST_PORT}/api/v1/regiones_states'.format(API_REST_HOST=API_REST_HOST, API_REST_PORT=API_REST_PORT)
@@ -46,9 +73,10 @@ def app_regiones_view(app):
             'menus' : menus,
             'regiones' : regiones,
         }
-        return render_template('admin/regiones.html', data=data)
+        return render_template('admin/regiones.html', data=data, message=message)
 
     def regiones_create(request):
+        print('create: ')
         try:
             name = request.form.get('name')
             latitude = request.form.get('latitude')
@@ -63,4 +91,34 @@ def app_regiones_view(app):
             response = requests.post(url, json=payload)
             print('response: ', response)
         except Exception as e:
+            traceback.print_exc()
+
+    def regiones_update(request):
+        print('update: ')
+        try:
+            id = request.form.get('id')
+            name = request.form.get('name')
+            latitude = request.form.get('latitude')
+            longitude = request.form.get('longitude')
+            payload = {
+                'id': id,
+                'name' : name,
+                'latitude' : latitude,
+                'longitude' : longitude
+            }
+            url = 'http://{API_REST_HOST}:{API_REST_PORT}/api/v1/regiones_states'.format(API_REST_HOST=API_REST_HOST, API_REST_PORT=API_REST_PORT)
+            print('url: ', url)
+            response = requests.put(url, json=payload)
+            print('response: ', response)
+        except Exception as e:
             traceback.print_exc()            
+        
+    def regiones_delete(id):
+        print('delete: ', id)
+        try:
+            url = 'http://{API_REST_HOST}:{API_REST_PORT}/api/v1/regiones_states/{id}'.format(API_REST_HOST=API_REST_HOST, API_REST_PORT=API_REST_PORT, id=id)
+            print('url: ', url)
+            response = requests.delete(url)
+            print('response: ', response)
+        except Exception as e:
+            traceback.print_exc()
